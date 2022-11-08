aa1859fa0f5cdd2acf4c14e41e816a36de896d65
Ondairos <ondairos@gmail.com>
2022-11-08 13:51:59 +0200

    added jsx for single movie display

--- src/App.js
+++ src/App.js
@@ -2,6 +2,17 @@ import React from 'react'
 import { useEffect } from 'react'
 import apiUrl from './helper.js'
 
+import './App.css'; //import css
+import SearchIcon from './search.svg';
+
+const movie1 = {
+    "Title": "Batman & Robin",
+    "Year": "1997",
+    "imdbID": "tt0118688",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BMGQ5YTM1NmMtYmIxYy00N2VmLWJhZTYtN2EwYTY3MWFhOTczXkEyXkFqcGdeQXVyNTA2NTI0MTY@._V1_SX300.jpg"
+}
+
 const App = () => {
 
     const searchMovies = async (title) => {
@@ -16,7 +27,39 @@ const App = () => {
     }, []);
 
     return (
-        <h1>App</h1>
+        <div className="app">
+            <h1>Flik Search</h1>
+
+            <div className='search'>
+                <input
+                    placeholder='Search for movies'
+                    value="Batman"
+                    onChange={() => { }}
+                />
+
+                <img
+                    src={SearchIcon}
+                    alt="search"
+                    onClick={() => { }}
+                />
+            </div>
+
+            <div className='container'>
+                <div className='movie'>
+                    <div>
+                        <p>{movie1.Year}</p>
+                    </div>
+                    <div>
+                        <img src={movie1.Poster !== 'N/A'? movie1.Poster: 'https://via.placeholder.com/400'} alt={movie1.Title}/>
+                    </div>
+
+                    <div>
+                        <span>{movie1.Type}</span>
+                        <h3>{movie1.Title}</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
