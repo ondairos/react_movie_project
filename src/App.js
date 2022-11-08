c1b3d65cefb975266fc33998df51bcb361c94d06
Ondairos <ondairos@gmail.com>
2022-11-08 13:13:47 +0200

    added searchMovies function through omdb API

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import React from 'react'
+import { useEffect } from 'react'
+import apiUrl from './helper.js'
 
 const App = () => {
-    return(
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${apiUrl}&s=${title}`); //template strings
+        const data = await response.json();
+
+        console.log(data.Search);
+    }
+
+    useEffect(() => {
+        searchMovies('Batman');
+    }, []);
+
+    return (
         <h1>App</h1>
     )
 }
