f81f02c9e1002492f1a05fb7cef92015c482f252
Ondairos <ondairos@gmail.com>
2022-11-08 12:51:05 +0200

    refactor src folder

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
-import { useState } from 'react';
-import './App.css';
+import React from 'react'
 
 const App = () => {
-  //useState hook
-  const [counter, setCounter] = useState(0);
-
-  return (
-    <div className="App">
-      <button onClick={() => setCounter((prevCount) => prevCount - 1)}>-</button>
-      <h1>{counter}</h1>
-      <button onClick={() => setCounter((prevCount) => prevCount + 1)}>+</button>
-    </div>
-  );
+    return(
+        <h1>App</h1>
+    )
 }
 
 export default App;
